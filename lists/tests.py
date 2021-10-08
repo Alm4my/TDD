bc373e4d4ccd48a2fc6e6cc16cee1717f1445594
Alm4my <thenixadm@gmail.com>
2021-10-08 02:00:33 +0000

    Implement a test with Django's Test Client

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,3 @@
-from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
@@ -11,11 +10,13 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_htm(self):
-        request = HttpRequest()
-        response = home_page(request)
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+
         html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.startswith('<html lang="en">'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
 
