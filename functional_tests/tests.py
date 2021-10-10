105f34a3ef11909bfb7bc30d3bec5dcb322c1033
Alm4my <thenixadm@gmail.com>
2021-10-10 06:08:19 +0000

    Refactor functional test to remove time.sleep()
    We removed the scattered time.sleep() and changed our check_for_row_in_list_table helper method to wait_for_row_in_list_table where we poll our app and return when the MAX_TIME has reached. It is better than the implicit waits.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,11 +4,13 @@
 # They should contain the user story
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
 
 class NewVisitorTest(LiveServerTestCase):
+    MAX_WAIT = 5
 
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
@@ -16,16 +18,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
-        # Unnecessary, but shows another way of doing it
-        self.assertTrue(
-            any(row.text == row_text for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        )
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise e
+                time.sleep(.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Mark has heard about a cool new online to-do app. He goes to check out
@@ -51,19 +55,17 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy a Burger" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
-        time.sleep(2)       # Explicit wait to make sure the browser has finish loading
-        self.check_for_row_in_list_table('1: Buy a Burger')
+        self.wait_for_row_in_list_table('1: Buy a Burger')
 
         # There is still a text box inviting him to add another item. He
         # enters "Use Burger's ketchup to make a soup"
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys("Use Burger's ketchup to make a soup")
         input_box.send_keys(Keys.ENTER)
-        time.sleep(2)
 
         # The page updates again, and now shows both items on his list
-        self.check_for_row_in_list_table('1: Buy a Burger')
-        self.check_for_row_in_list_table("2: Use Burger's ketchup to make a soup")
+        self.wait_for_row_in_list_table('1: Buy a Burger')
+        self.wait_for_row_in_list_table("2: Use Burger's ketchup to make a soup")
 
         # Mark wonders whether the site will remember his list. Then he sees
         # that the site has generated a unique URL for him -- there is some
