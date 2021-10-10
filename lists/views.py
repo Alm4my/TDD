4782bab3e0b93d1eedc69667ee5c1f0a91595b7b
Alm4my <thenixadm@gmail.com>
2021-10-10 00:47:30 +0000

    Modify views.py to allow a new item to be saved in the database

--- lists/views.py
+++ lists/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home_page(request):
-    return render(request,
-                  'home.html',
-                  {
-                      'new_item_text': request.POST.get('item_text', '')
-                  })
+from lists.models import Item
+
+
+def home_page(request):   # TODO: Take care of the empty saves to the database
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+    return render(
+        request,
+        'home.html',
+        {'new_item_text': request.POST.get('item_text', '')}
+    )
