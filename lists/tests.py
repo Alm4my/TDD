ef22c2b4c33df07b80c4524c28b6105f92dcc368
Alm4my <thenixadm@gmail.com>
2021-10-10 07:31:48 +0000

    Add new unit tests for list view, url for one list, and remove test_displays_all_list_items()

--- lists/tests.py
+++ lists/tests.py
@@ -25,20 +25,11 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-app/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('item 1', response.content.decode())
-        self.assertIn('item 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -60,3 +51,14 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Second Item')
 
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-app/')
+
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
--- lists/views.py
+++ lists/views.py
@@ -8,7 +8,7 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-app/')
 
     items = Item.objects.all()
     return render(
@@ -16,3 +16,8 @@ def home_page(request):
         'home.html',
         {'items': items}
     )
+
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
