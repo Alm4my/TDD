00847b92a0cb2ea4c4fa98b778df8a8f35257c8d
Alm4my <thenixadm@gmail.com>
2021-10-11 08:14:20 +0000

    Add foreign key to our item class to have each item belong to a list

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,12 @@
+from django.db.models import Model
 from django.db import models
 
 
 # Create your models here.
-class Item(models.Model):
+class List(Model):
+    pass
+
+
+class Item(Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
